05100ca1a8e1241198c3c083d94676833d2e69fb
saibat01 <mohammedmehdisaibat@gmail.com>
2025-05-08 16:15:23 +0100

    solved

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,33 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  if (
+    typeof start !== "number" ||
+    typeof end !== "number" ||
+    !Number.isInteger(start) ||
+    !Number.isInteger(end) ||
+    start < 0 ||
+    end < 0
+  ) {
+    return "ERROR";
+  }
 
+  if (start > end) {
+    [start, end] = [end, start];
+  }
+
+  let bigStart = BigInt(start);
+  let bigEnd = BigInt(end);
+  let sum = 0n;
+
+  for (let i = bigStart; i <= bigEnd; i++) {
+    sum += i;
+  }
+
+  const MAX_SAFE = BigInt(Number.MAX_SAFE_INTEGER);
+  if (sum <= MAX_SAFE) {
+    return Number(sum);
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
