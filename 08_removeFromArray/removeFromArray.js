133d69f7be88476225b3b83894a0041f0b0fad20
saibat01 <mohammedmehdisaibat@gmail.com>
2025-05-08 10:16:10 +0100

    solved

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  let result = [];
+  for (let i = 0; i < array.length; i++) {
+    if (args.includes(array[i])) {
+      continue;
+    } else {
+      result.push(array[i]);
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
