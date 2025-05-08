5df5da74dc76d66d983301abded9b3c0f281ce34
saibat01 <mohammedmehdisaibat@gmail.com>
2025-05-08 09:39:51 +0100

    solved

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let array = string.split("");
+  let reversedString = [];
+  for (let i = 0; i < array.length; i++) {
+    reversedString.unshift(array[i]);
+  }
 
+  return reversedString.join("");
 };
+
+// LOOPING THROUGH STRING:
+
+// const reverseString = function(string) {
+//     let reversedString = [];
+//     for(let i = 0; i < string.length; i++) {
+//         reversedString.unshift(string.charAt(i));
+//     }
+
+//     return reversedString.join("");
+// };
 
 // Do not edit below this line
 module.exports = reverseString;
