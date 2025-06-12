34c07462dd5c73c856537857053ec6c633c124db
saibat01 <mohammedmehdisaibat@gmail.com>
2025-06-12 16:47:40 +0100

    solved

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,50 @@
-const palindromes = function () {
+const punctuationMarks = [
+  ".", // period
+  ",", // comma
+  "!", // exclamation mark
+  "?", // question mark
+  ":", // colon
+  ";", // semicolon
+  "-", // hyphen
+  "—", // em dash
+  "(", // left parenthesis
+  ")", // right parenthesis
+  "[", // left bracket
+  "]", // right bracket
+  "{", // left brace
+  "}", // right brace
+  "'", // single quote
+  '"', // double quote
+  "`", // backtick
+  "…", // ellipsis
+  "/", // slash
+  "\\", // backslash
+  "@", // at symbol
+  "#", // hash
+  "$", // dollar sign
+  "%", // percent
+  "&", // ampersand
+  "*", // asterisk
+  "^", // caret
+  "=", // equals sign
+  "+", // plus
+  "<", // less than
+  ">", // greater than
+  "|", // vertical bar
+  "~", // tilde
+  " ",
+];
 
+const palindromes = function (string) {
+  const cleaned = string
+    .toLowerCase()
+    .split("")
+    .filter((char) => !punctuationMarks.includes(char))
+    .join("");
+
+  const reversed = cleaned.split("").reverse().join("");
+
+  return cleaned === reversed;
 };
 
 // Do not edit below this line
