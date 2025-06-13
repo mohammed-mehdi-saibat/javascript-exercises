609a98ea9f4979ef8f1467cc35cdebc94fb087e0
saibat01 <mohammedmehdisaibat@gmail.com>
2025-06-13 15:46:18 +0100

    solved

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  n = +n;
+  if (n < 0) return "OOPS";
+  if (n === 0) return 0;
+  if (n === 1 || n === 2) return 1;
 
+  let a = 1;
+  let b = 1;
+  let next;
+
+  for (let i = 3; i <= n; i++) {
+    next = a + b;
+    a = b;
+    b = next;
+  }
+  return next;
 };
 
 // Do not edit below this line
